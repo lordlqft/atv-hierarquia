fa1ab5b00c8494832186d72e6bfb67db15f9e79a
Kenneth <lordlqft0@gmail.com>
2026-09-10 16:40:51 -0300

    Feat: Herança Produto
    Classe PAI Produto que herda para ProdutoDigital e ProdutoFisico

---
+++ Produto.cs
@@ -0,0 +1,20 @@
+public class Produto
+{
+    private string _nome = "";
+    private decimal _preco = 0;
+
+    public Produto(string nome, decimal preco)
+    {
+        Nome = nome;
+        Preco = preco;
+    }
+
+    public string Nome { get { return _nome; } set { _nome = value; } }
+    public decimal Preco { get { return _preco; } set { _preco = value; } }
+
+    public void ExibirDados()
+    {
+        Console.WriteLine($"Produto: {Nome}");
+        Console.WriteLine($"Preço: R$ {Preco:F2}");
+    }
+}
