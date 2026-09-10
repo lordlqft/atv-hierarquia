6babf3037888128eef3783eca4edfe63d8e1285a
Kenneth <lordlqft0@gmail.com>
2026-09-10 16:44:18 -0300

    Feat: Instanceamento de objetos
    Instanceando e testando os atributos do ProdutoFisico e ProdutoDigital

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// ProdutoDigital produtoDigital = new ProdutoDigital(
+//     "Curso de C#",
+//     50,
+//     "www.exemplo.com/download"
+// );
+
+// produtoDigital.ExibirDados();
+// produtoDigital.LiberarDownload();
+
+ProdutoFisico produto = new ProdutoFisico("Caderno", 30, 2, 1);
+
+decimal frete = produto.CalcularFrete(produto.Peso);
+
+Console.WriteLine($"Frete: R$ {frete:F2}");
